7cbdc0241690e325d043c9b2ed56e4bb8b767157
zhangsan <zhangsan.cn>
2023-02-08 13:36:09 +0800

    输入输出练习

---
+++ 01-Python基础/test01_helloworld.py
@@ -0,0 +1,17 @@
+# encoding=utf-8
+
+"""
+Helloworld
+author：LBL
+date:2023-2-8
+"""
+
+if __name__ == "__main__":
+    # 输入，默认都是字符串
+    num = int(input("输入要循环的次数"))
+    i = 0
+    while i < num:
+        print("HelloWorld")
+        i += 1
+
+
