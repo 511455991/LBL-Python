d97980eedbb1b1cbfdc862f3c4837d1c5f762e7a
511455991 <105531802+511455991@users.noreply.github.com>
2023-02-15 14:28:58 +0800

    Update command_linux_shell.sh
    linux中shell脚本的使用

--- 05-Python开发/command_linux_shell.sh
+++ 05-Python开发/command_linux_shell.sh
@@ -27,12 +27,12 @@
 comment
 
 # 输入。read读取
-read -t5 -p ‘请输入两个数字’ A B	# read读取用户输入-t5秒-p提示信息
+read -t5 -p ‘请输入两个数字’ A B	 # read读取用户输入-t5秒-p提示信息
 [ -z $A ] && A=100			# 如果A为空，就将A赋值100
-echo “$A+$B的结果是$[$A+$B]”	# 输出A+B结果
+echo “$A+$B的结果是$[$A+$B]”	    # 输出A+B结果
 
-# 输出。				echo打印，打印变量。printf打印，与echo类似，有些不同
-echo 脚本开始			# 打印字符串	
+# 输出。			     echo打印，打印变量。printf打印，与echo类似，有些不同
+echo 脚本开始		    # 打印字符串	
 echo $1				# $n为位置参数。即执行脚本时传递的参数，n为数字
 echo $0				# $0为当前脚本文件名
 echo $*				# $*为脚本所有参数
@@ -44,7 +44,7 @@ echo $$				# $$为当前进程的PID
 # 变量属性
 declare			# declare用来声明变量，declare声明的默认是局部变量
 			# -g声明全局变量
-declare -i SUM=0		# -i声明整数变量
+declare -i SUM=0	# -i声明整数变量
 			# -a声明数组变量
 			# -r声明只读变量
 			# -p显示定义变量的语句，即属性和值
@@ -59,34 +59,34 @@ echo $log
 unset log				# 删除变量,不能删除只读变量
 
 # 字符串变量。			shell中变量默认值都是字符串，=两边不能有空格，变量使用才需要加$，重新赋值不用
-NAME=“北京张三的北京弟弟”	# 定义变量
+NAME=“北京张三的北京弟弟”	  # 定义变量
 echo “${NAME}在工作”		# ${}输出变量。{}可省略，不建议省略。   		北京张三的北京弟弟在工作
-echo ‘${NAME}在工作’		# 单引号强引用，所见即所得。 			${NAME}在工作
-echo  ${#NAME} 			# ${#}获取字符串长度
-echo  ${NAME:0:2}			# 从左截取字符串，${string:begin:length}		北京
-echo  ${NAME:6}			# 省略length可截取到末尾。			弟弟
-echo  ${NAME:0-2:2}			# 从右截取字符串，${string:0-begin:length}  	弟弟
-echo  ${NAME:0-2}			# 省略length，从右开始截取到末尾		弟弟
-echo  ${NAME#*京}			# ${string#}从头匹配指定字符串后的内容		张三的北京弟弟
-echo  ${NAME##*北京}		# ${string##}从尾匹配指定字符串后的内容。	弟弟
-echo  ${NAME%*张三}			# ${string%}匹配指定字符串前的内容	北京
+echo ‘${NAME}在工作’		# 单引号强引用，所见即所得。 			  ${NAME}在工作
+echo  ${#NAME} 			   # ${#}获取字符串长度
+echo  ${NAME:0:2}		   # 从左截取字符串，${string:begin:length}		  北京
+echo  ${NAME:6}			   # 省略length可截取到末尾。				 弟弟
+echo  ${NAME:0-2:2}		   # 从右截取字符串，${string:0-begin:length}  		  弟弟
+echo  ${NAME:0-2}		   # 省略length，从右开始截取到末尾			      弟弟
+echo  ${NAME#*京}		  # ${string#}从头匹配指定字符串后的内容		    张三的北京弟弟
+echo  ${NAME##*北京}		 # ${string##}从尾匹配指定字符串后的内容。	          弟弟
+echo  ${NAME%*张三}		 # ${string%}匹配指定字符串前的内容	             北京
 
 # 数组变量
-nums=(11 243 23 )			# 定义数组用(),元素使用空格分割
-nums+=(7 “小明”)			# 添加元素。				11 243 23 7 小明
-nums[2]=45			# 修改元素。				11 243 45 7 小明
-unset ${nums[2]}			# 删除数组中元素				11 243 7 小明
-echo ${nums[0]}			# 获取数组中元素				11
-echo ${nums[*]}			# 获取数组所有元素。$arr[*]或者$arr[@]		11 243 45 7 小明
-echo ${#nums[*]}			# 获取数组个数。				5
-echo ${#nums[1]}			# 获取数组中某元素长度。			3
+nums=(11 243 23 )		  # 定义数组用(),元素使用空格分割
+nums+=(7 “小明”)			# 添加元素。					11 243 23 7 小明
+nums[2]=45			  # 修改元素。				  11 243 45 7 小明
+unset ${nums[2]}		  # 删除数组中元素				11 243 7 小明
+echo ${nums[0]}			  # 获取数组中元素				11
+echo ${nums[*]}			  # 获取数组所有元素。$arr[*]或者$arr[@]	    11 243 45 7 小明
+echo ${#nums[*]}		  # 获取数组个数。				5
+echo ${#nums[1]}		  # 获取数组中某元素长度。			    3
 nums1=(11 12)
 nums2=(13 14)
-nums3=($nums1[*] $nums2[*])		# 数组拼接
+nums3=($nums1[*] $nums2[*])	  # 数组拼接
 
 
 # 运算符+  -  *  /  %  **  ++  --  +=  -+  *=  /=
-num=`expr 2+2`			# 整数运算，bash本身变量都当成字符串不支持计算，expr是计算命令
+num=`expr 2+2`				# 整数运算，bash本身变量都当成字符串不支持计算，expr是计算命令
 num2=$(( 10 - 8 ))			# 整数运算，(( ))同expr。取结果需要加$
 num2=$((i=i+10))			# (())会自动解析内部变量，里面的变量计算时不用加$
 num3=`bc 1.1+1.2`			# bc是外部计算器，可小数运算，shell本身只可以整数计算
@@ -138,12 +138,12 @@ if [ -z "$str1" -o -z "$str2" ];then			# 使用""将变量包裹可避免奇葩�
     exit 0
 fi
 
-if [[ -z $str1  ||  -z str2 ]];then				# 效果同上。   
+if [[ -z $str1  ||  -z str2 ]];then			# 效果同上。   
     echo "字符串不能为空”"	
     exit 0
 fi
 
-if [[ $tel =~ ^1[0-9]{10}$ ]];then				# 举例：检测tel变量是否符合正则匹配的手机号格式
+if [[ $tel =~ ^1[0-9]{10}$ ]];then			# 举例：检测tel变量是否符合正则匹配的手机号格式
 	echo "是手机号"
 fi
 
@@ -165,8 +165,8 @@ case $1 in
     echo "分支遇到;;就结束啦";;			# ;;相当于while语句的break
 [a-z])				
     echo "是小写字母";;		
-*）					# 情况都没匹配上就会执行*)，*)相当于if语句的else
-    echo "其他字符"				# 最后*)分支可以不用;;结尾，因为本身执行到esac也结束了
+*）						# 情况都没匹配上就会执行*)，*)相当于if语句的else
+    echo "其他字符"				     # 最后*)分支可以不用;;结尾，因为本身执行到esac也结束了
 esac
 
 
@@ -186,7 +186,7 @@ i=1;sum=0				# until在条件不成立时才会循环。与while相反
 until (( i>100 ));do
     (( sum+=i ))
     (( i++ ))
-    if sum >200 ;break 1			# break n用来跳出循环,n为循环层数，不写为所有循环
+    if sum >200 ;break 1		# break n用来跳出循环,n为循环层数，不写为所有循环
 done					# continue n 用来跳过几次循环，默认一次
 echo "sum:$sum"
 
@@ -234,7 +234,7 @@ a
 function func(){
 	echo “language: $1”
 	echo “URL: $2”
-	retrun `expr 1+1` 				# retrun为函数返回值。expr数学运算
+	retrun `expr 1+1` 			# retrun为函数返回值。expr数学运算
 }
 func C++ http://www.baidu.com			# 调用函数，传两个位置参数。
 echo $?						# 获取函数返回值。
@@ -242,7 +242,7 @@ echo $?						# 获取函数返回值。
 
 
 # 代码块，开始符和结束符之间的内容是一个整体，可以直接打印、存入变量、
-cat <<EOF							# 举例：显示一个菜单给用户，让用户输入字母选择功能。
+cat <<EOF								# 举例：显示一个菜单给用户，让用户输入字母选择功能。
 d|D) show disk usages.	<<EOF代码块，EOF为结束符，当输入内容为EOF时结束
 m|M) show memory usages.
 s|S) show swap usages.
@@ -256,7 +256,7 @@ select * from students where id<10;
 exit;
 EOF
 
-tee /etc/a.txt <<'EOF'						# 举例:将代码块内容写入到a.txt文件中。tee命令是从标准输入读取内容写入文件或标准输出							
+tee /etc/a.txt <<'EOF'							# 举例:将代码块内容写入到a.txt文件中。tee命令是从标准输入读取内容写入文件或标准输出							
  这些是被写入文件的内容
  
 EOF
