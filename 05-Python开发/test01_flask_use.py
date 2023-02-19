ac60d88a61f29940d671d482b3b3be3a303689a7
lbl <lbl.cn>
2023-02-19 08:30:22 +0800

    flask开发框架基础使用

---
+++ 05-Python开发/test01_flask_use.py
@@ -0,0 +1,21 @@
+# encoding=utf-8
+
+"""
+Flask开发框架基础使用，
+author：LBL
+date:2023-2-19
+"""
+
+
+class MyTest(unittest.TestCase):
+    pass
+
+# 程序主入口
+if __name__ == "__main__":
+    pass
+
+
+
+
+
+
