960897e977d6655a2c912e6bb4de8ac77218443b
lbl <lbl.cn>
2023-02-24 13:11:32 +0800

    自动化测试框架基础使用

--- 03-测试/test03_framework_use.py
+++ 03-测试/test03_framework_use.py
@@ -1,17 +1,30 @@
 # encoding=utf-8
 
 """
-framework测试框架基础使用，
+自动化测试框架基础使用
 author：LBL
-date:2023-2-19
+date:2023-2-24
 """
 
 
-class MyTest(unittest.TestCase):
-    pass
 
 # 程序主入口
 if __name__ == "__main__":
+    """ framework即框架，是用来解决特定问题，配置的一套由解决问题的组件构成的可扩展工具
+     常见的自动化测试框架：编程语言 + 单元测试框架 + 持续集成工具 + 数据库 + 测试项目管理工具
+     python + selenium/appium + unittest/pytest + git + jenkins + mysql + testlink/redmine
+     python + rebot framework + unittest/pytest + git + jenkins + mysql + testlink/redmine
+     文件结构：
+        log：存放日志
+        report：存放测试报告
+        source：存放配置文件、静态资源
+        common：存放公共函数、方法及通用操作
+        testcase：存放测试用例
+        testdata：存放测试数据，数据和脚本分离
+        testsuite：存放测试组件
+        statistics：存放测试结构统计
+        continuous：持续集成环境
+     """
     pass
 
 
